93ee6a116926fa41cc90baa951d7b2d4f5eddf94
Nur_Adnan <nuradnanchowdhuryestobdho@gmail.com>
2024-10-07 18:50:45 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import SignupPage from "./pages/SignupPage";
 import SigninPage from "./pages/SigninPage";
 import WelcomePage from "./pages/WelcomePage";
 import LearnerDashboard from "./pages/LearnerDashboard";
+// import IssuerDashboard from "./pages/IssuerDashboard";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/signin" element={<SigninPage />} />
         <Route path="/learner-dashboard" element={<LearnerDashboard />} />
+        {/* <Route path="/issuer-dashboard" element={<IssuerDashboard />} /> */}
         <Route path="/" element={<WelcomePage />} />
       </Routes>
     </Router>
