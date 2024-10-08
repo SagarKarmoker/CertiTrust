b4d92921c6e498870d6226c9f58a770bf3568bfd
Sagar Karmoker <sagarkarmoker2001@gmail.com>
2024-10-09 00:53:25 +0600

    rainbow setup

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,49 @@ import WelcomePage from "./pages/WelcomePage";
 import LearnerDashboard from "./pages/LearnerDashboard";
 import IssuerDashboard from "./pages/IssuerDashboard";
 import EmployerDashboard from "./pages/EmployerDashboard";
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultConfig,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { WagmiProvider } from 'wagmi';
+import {
+  baseSepolia
+} from 'wagmi/chains';
+import {
+  QueryClientProvider,
+  QueryClient,
+} from "@tanstack/react-query";
+
+
+const config = getDefaultConfig({
+  appName: 'CertiTrust',
+  projectId: '0e276d0f57e30fea190f8962759a6815',
+  chains: [baseSepolia],
+  ssr: true, // If your dApp uses server side rendering (SSR)
+});
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/signin" element={<SigninPage />} />
-        <Route path="/learner-dashboard" element={<LearnerDashboard />} />
-        <Route path="/issuer-dashboard" element={<IssuerDashboard />} />
-        <Route path="/employer-dashboard" element={<EmployerDashboard />} />
-        <Route path="/" element={<WelcomePage />} />
-      </Routes>
-    </Router>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>
+          <Router>
+            <Routes>
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/signin" element={<SigninPage />} />
+              <Route path="/learner-dashboard" element={<LearnerDashboard />} />
+              <Route path="/issuer-dashboard" element={<IssuerDashboard />} />
+              <Route path="/employer-dashboard" element={<EmployerDashboard />} />
+              <Route path="/" element={<WelcomePage />} />
+            </Routes>
+          </Router>
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+
   );
 }
 
